358813886bcc79acaf2e078c0860bd3b3ded6e18
Aman <ammuaman75@gmail.com>
2026-03-08 17:34:23 +0530

    JWT done.

--- 17-URLshortner/service/auth.js
+++ 17-URLshortner/service/auth.js
@@ -1,11 +1,20 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
+const secret = 'ViratKohli18'
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user)
+
+function setUser(user){
+    // sessionIdToUserMap.set(id,user)
+    return jwt.sign({
+        _id: user._id,
+        email : user.email
+    } , secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    // return sessionIdToUserMap.get(id);
+    if (!token) return null;
+    return jwt.verify(token,secret);
 }
 
 module.exports = {
